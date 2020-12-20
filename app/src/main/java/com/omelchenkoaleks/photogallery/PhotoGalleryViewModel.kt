b5c35ed607770efdefc60345dca77316672fbb69
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-20 16:35:42 +0200

    Получаем данные лишь раз при запуске приложения и сохраняем их во ViewModel. (Нужно для того, чтобы данные не загружались снова и снова - например - при смене конфигурации устройства). Подписываемся на изменения.

---
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,17 @@
+package com.omelchenkoaleks.photogallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel() {
+
+    /**
+     * Свойство для хранения объекта "живых" данных -
+     * содержит список элементов галереи.
+     */
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+    }
+}
