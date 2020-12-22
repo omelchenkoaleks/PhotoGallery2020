624071f98de3fc2f432da3a028d60a1f9d0150cb
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-22 10:05:55 +0200

    Абстрагируем запуск и стоп потока в специально созданном классе Downloader. Использует lifecycleObserver для связи с жизненным циклом владельца (Fragment).

--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,12 +21,17 @@ class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        retainInstance = true
+
         photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
 
+        thumbnailDownloader = ThumbnailDownloader()
+        lifecycle.addObserver(thumbnailDownloader)
     }
 
     override fun onCreateView(
@@ -49,6 +53,13 @@ class PhotoGalleryFragment : Fragment() {
                 Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
                 photoRecyclerView.adapter = PhotoAdapter(galleryItems)
             }
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(
+            thumbnailDownloader
         )
     }
 
