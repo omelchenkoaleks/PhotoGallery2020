2d64a587a8a3e124b3c1955e569cda839767444c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-22 09:14:22 +0200

    Функция в интерфейс api-retrofit для проддержки загрузки изображения. Инкапсуляция загрузки в репозитории.

--- app/src/main/java/com/omelchenkoaleks/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/FlickrFetchr.kt
@@ -1,11 +1,15 @@
 package com.omelchenkoaleks.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.omelchenkoaleks.photogallery.api.FlickrApi
 import com.omelchenkoaleks.photogallery.api.FlickrResponse
 import com.omelchenkoaleks.photogallery.api.PhotoResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -70,6 +74,18 @@ class FlickrFetchr {
         })
 
         return responseLiveData
+    }
+
+    /**
+     * Аннотация указывает, что функция должна выполняться только в фоновом потоке.
+     * Создать фоновый поток - уже наша задача.
+     */
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 
 }
