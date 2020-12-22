32fc2237ca5318cada476aefd23486b0f45ada51
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-22 10:52:57 +0200

    Использование Handler.

--- app/src/main/java/com/omelchenkoaleks/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/ThumbnailDownloader.kt
@@ -1,17 +1,40 @@
 package com.omelchenkoaleks.photogallery
 
+import android.annotation.SuppressLint
+import android.os.Handler
 import android.os.HandlerThread
+import android.os.Message
 import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
 
 class ThumbnailDownloader<in T>
     : HandlerThread(TAG), LifecycleObserver {
 
     private var hasQuit = false
+
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetchr = FlickrFetchr()
+
+    @Suppress("UNCHECKED_CAST")
+    @SuppressLint("HandlerLeak")
+    override fun onLooperPrepared() {
+        requestHandler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    val target = msg.obj as T
+                    Log.i(TAG, "Got a request for URL: ${requestMap[target]}")
+                    handleRequest(target)
+                }
+            }
+        }
+    }
 
     override fun quit(): Boolean {
         hasQuit = true
@@ -35,5 +58,13 @@ class ThumbnailDownloader<in T>
 
     fun queueThumbnail(target: T, url: String) {
         Log.i(TAG, "Got a URL: $url")
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+            .sendToTarget()
+    }
+
+    private fun handleRequest(target: T) {
+        val url = requestMap[target] ?: return
+        val bitmap = flickrFetchr.fetchPhoto(url) ?: return
     }
 }
