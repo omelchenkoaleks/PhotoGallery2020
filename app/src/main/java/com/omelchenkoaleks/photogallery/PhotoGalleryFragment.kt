3320e0367d8329673cf3f763b72d16ff8ab831f2
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-22 11:11:17 +0200

    Передача обработчика.

--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
@@ -1,8 +1,10 @@
 package com.omelchenkoaleks.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +32,12 @@ class PhotoGalleryFragment : Fragment() {
 
         photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
 
-        thumbnailDownloader = ThumbnailDownloader()
+        val responseHandler = Handler()
+        thumbnailDownloader =
+            ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+                val drawable = BitmapDrawable(resources, bitmap)
+                photoHolder.bindDrawable(drawable)
+            }
         lifecycle.addObserver(thumbnailDownloader)
     }
 
--- app/src/main/java/com/omelchenkoaleks/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/ThumbnailDownloader.kt
@@ -1,6 +1,7 @@
 package com.omelchenkoaleks.photogallery
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Message
@@ -13,8 +14,10 @@ import java.util.concurrent.ConcurrentHashMap
 private const val TAG = "ThumbnailDownloader"
 private const val MESSAGE_DOWNLOAD = 0
 
-class ThumbnailDownloader<in T>
-    : HandlerThread(TAG), LifecycleObserver {
+class ThumbnailDownloader<in T>(
+    private val responseHandler: Handler,
+    private val onThumbnailDownloaded: (T, Bitmap) -> Unit
+) : HandlerThread(TAG), LifecycleObserver {
 
     private var hasQuit = false
 
@@ -66,5 +69,13 @@ class ThumbnailDownloader<in T>
     private fun handleRequest(target: T) {
         val url = requestMap[target] ?: return
         val bitmap = flickrFetchr.fetchPhoto(url) ?: return
+
+        responseHandler.post(Runnable {
+            if (requestMap[target] != url || hasQuit) {
+                return@Runnable
+            }
+            requestMap.remove(target)
+            onThumbnailDownloaded(target, bitmap)
+        })
     }
 }
