ab8980df844302de2d6af2c00f3f0e8dad7cd396
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-22 13:41:16 +0200

    Заполнение текстрового поля поиска сохраненным запросом.

--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
@@ -102,6 +102,10 @@ class PhotoGalleryFragment : Fragment() {
                     return false
                 }
             })
+
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
         }
     }
 
