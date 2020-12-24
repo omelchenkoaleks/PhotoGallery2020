4df3c438288609387bd57f3db8911fe12ecde84a
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-24 15:35:29 +0200

    PhotoPageActivity.kt PhotoPageFragment.kt для отображения фотографии.

--- app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/photogallery/PhotoGalleryFragment.kt
@@ -177,7 +177,8 @@ class PhotoGalleryFragment : VisibleFragment() {
         }
 
         override fun onClick(view: View) {
-            val intent = Intent(Intent.ACTION_VIEW, galleryItem.photoPageUri)
+            val intent = PhotoPageActivity
+                .newIntent(requireContext(), galleryItem.photoPageUri)
             startActivity(intent)
         }
     }
